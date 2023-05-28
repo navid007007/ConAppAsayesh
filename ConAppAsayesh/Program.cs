6836d0053e6d5a3957cee6875ae7307e6d2f5d9a
S07C05 <S07C05@S07C05>
2023-05-28 09:20:01 +0430

    first commit

--- ConAppAsayesh/Program.cs
+++ ConAppAsayesh/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = a + b;
